2b4cc048790d3e2cd83e3a8a130c205aedbb55c4
JM-Dev <jtogni.developer@gmail.com>
2023-11-24 20:21:25 -0300

    build

---
+++ .netlify/functions-internal/chunks/generic_2e9feb41.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/generic_522b5f76.mjs');
+
+export { page };
