84cde41ba6bfd7de9807632502675db3c24107e3
JM-Dev <jtogni.developer@gmail.com>
2023-11-24 22:50:48 -0300

    cambio en fetch

--- .netlify/functions-internal/entry.mjs
+++ .netlify/functions-internal/entry.mjs
@@ -1,9 +1,9 @@
 import * as adapter from '@astrojs/netlify/netlify-functions.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_8f9096fa.mjs';
+import { manifest } from './manifest_510e24cb.mjs';
 
 const _page0  = () => import('./chunks/generic_2e9feb41.mjs');
-const _page1  = () => import('./chunks/index_f508767d.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1]]);
+const _page1  = () => import('./chunks/index_e01ce01c.mjs');const pageMap = new Map([["node_modules/astro/dist/assets/endpoint/generic.js", _page0],["src/pages/index.astro", _page1]]);
 const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
