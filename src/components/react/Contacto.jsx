322d21cec250a4eeffb61cd672d83e90fdd83d27
JM-Dev <jtogni.developer@gmail.com>
2023-11-25 17:38:46 -0300

    modicaciones en rutas

--- src/components/react/Contacto.jsx
+++ src/components/react/Contacto.jsx
@@ -19,10 +19,10 @@ const Contacto = () => {  const [showAutomotoresForm, setShowAutomotoresForm] = 
     <div class="min-h-[400PX]  h-auto grid bg-black w-full">
         <div class="flex flex-col  items-center sm:justify-center md:justify-start flex-auto ">
             <div class="relative sm:w-1/2 xl:w-3/5  h-full  md:flex flex-auto flex-col items-center justify-center p-4 overflow-hidden text-white gap-6">
-                <div class="w-full">
+                <div  class="w-full">
                     <p class="text-6xl text-slate-100 text-center mb-6">Solicita tu Cotización</p>
                 </div>
-                <div class="flex gap-5 flex-col md:flex-row w-full justify-center items-center">
+                <div  class="flex gap-5 flex-col md:flex-row w-full justify-center items-center">
                     <button class="btn glass" onClick={handleAutomotoresButtonClick} >Automotores</button>
                     <button class="btn glass" onClick={handleOtrosRiesgosButtonClick}>Otros Riesgos</button>
                 </div>                
