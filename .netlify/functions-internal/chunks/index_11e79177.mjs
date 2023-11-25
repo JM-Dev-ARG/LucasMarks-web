1e57c4ff2deb0eb7e632eca41a23ab31cc43a531
JM-Dev <jtogni.developer@gmail.com>
2023-11-25 00:13:17 -0300

    ajuste build

---
+++ .netlify/functions-internal/chunks/index_11e79177.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_9688befe.mjs');
+
+export { page };
---
+++ .netlify/functions-internal/chunks/index_b8c15f69.mjs
@@ -0,0 +1,6 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+
+const page = () => import('./pages/index_af1a99fe.mjs');
+
+export { page };
